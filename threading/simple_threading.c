cb021543bcb04200318317d098396a80c2e527f5
Taks Joseph Refugio <taksjosephrefugio@gmail.com>
2022-06-29 01:09:27 -0500

    create array of threads
    Signed off by: Taks Joseph Refugio <taksjosephrefugio@gmail.com>

--- threading/simple_threading.c
+++ threading/simple_threading.c
@@ -2,15 +2,25 @@
 #include<pthread.h>
 #include <unistd.h>     // sleep()
 
+#define NUM_THREADS 5
+
+
 // FUNCTION PROTOTYPES
 void *job(void *);
 
 
 void main() {
-    int sleeptime = 1;
-    pthread_t single_thread;
-    pthread_create(&single_thread, NULL, job, (void *) &sleeptime);
-    pthread_join(single_thread, NULL);
+    pthread_t threadlist[NUM_THREADS];
+
+    // Creating threads
+    for (int ctr = 1; ctr <= NUM_THREADS; ctr++) {
+        pthread_create(&threadlist[ctr-1], NULL, job, (void *) &ctr);
+    }
+
+    // Joining all threads
+    for (int i = 0; i < 5; i++) {
+        pthread_join(threadlist[i], NULL);
+    }
 }
 
 
@@ -18,4 +28,5 @@ void *job(void *time_in_seconds) {
     int sleeptime = *(int *)time_in_seconds;
     printf(">>> Sleeping for %d seconds...\n", sleeptime);
     sleep(sleeptime);
+    printf(">>> Done sleeping for %d seconds...\n", sleeptime);
 }
